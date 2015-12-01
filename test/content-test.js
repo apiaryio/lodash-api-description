de2dd27d248f18dda4b1ed884713091e9b4da280
Ondřej Musil <ondra@apiary.io>
2015-12-01 10:13:40 +0100

    Handling primitives in content function

--- test/content-test.js
+++ test/content-test.js
@@ -4,19 +4,54 @@ var apiDescription = require('../lib/index');
 apiDescription(lodash);
 
 describe('Content Function', function() {
-  context('when content exists', function() {
+  context('when content exists as refract element', function() {
     var refract = {
-      element: 'category',
-      content: [
-        {
-          element: 'resource',
-          content: []
+      "element": "category",
+      "meta": {
+        "classes": [
+          "api"
+        ],
+        "title": {
+          "element": "string",
+          "meta": {},
+          "attributes": {
+            "sourceMap": [
+              {
+                "element": "sourceMap",
+                "meta": {},
+                "attributes": {},
+                "content": [
+                  [
+                    40,
+                    23
+                  ]
+                ]
+              }
+            ]
+          },
+          "content": "Swagger Petstore"
         }
-      ]
+      }
+    }
+
+    it('returns the content of the element', function() {
+      expect(lodash.chain(refract).get('meta.title', '').content().value()).to.deep.equal('Swagger Petstore');
+    });
+  });
+
+  context('when content exists as primitive value', function() {
+    var refract = {
+      "element": "category",
+      "meta": {
+        "classes": [
+          "api"
+        ],
+        "title": "Swagger Petstore"
+      }
     };
 
     it('returns the content of the element', function() {
-      expect(lodash.content(refract)).to.deep.equal(refract.content);
+      expect(lodash.chain(refract).get('meta.title', '').content().value()).to.deep.equal('Swagger Petstore');
     });
   });
 });
